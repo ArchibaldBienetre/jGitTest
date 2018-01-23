cda2b01e1d2c7967023577e77a984e12e058c1c0
hoobert <bluewhobert@gmail.com>
2018-01-23 22:12:49 +0100

    GitWrapper: wrap git-branch and some helper functions

--- src/main/java/com/example/jgit/GitWrapper.java
+++ src/main/java/com/example/jgit/GitWrapper.java
@@ -1,15 +1,19 @@
 package com.example.jgit;
 
-import com.google.common.collect.Iterables;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
 
 /**
  * Encapsulates a GIT repository in the file system using <a href="http://wiki.eclipse.org/JGit/User_Guide">jGit</a>
@@ -22,10 +26,7 @@ public class GitWrapper {
      * Create or open a GIT repository at the given directory
      */
     public GitWrapper(File directory) throws IOException, GitAPIException {
-        FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        builder.setWorkTree(directory);
-//                .readEnvironment() // scan environment GIT_* variables
-//                .findGitDir(); // scan up the file system tree
+        FileRepositoryBuilder builder = new FileRepositoryBuilder().setWorkTree(directory);
         File hiddenGitDir = builder.getGitDir();
         if (hiddenGitDir == null || !hiddenGitDir.exists()) {
             Git.init().setBare(false).setDirectory(directory).call();
@@ -57,7 +58,7 @@ public class GitWrapper {
      * @return successful commit's SHA-1, followed by the commit's short message
      */
     public String getLastLogEntry() throws GitAPIException {
-        RevCommit logEntry = Iterables.getOnlyElement(_git.log().setMaxCount(1).call());
+        RevCommit logEntry = getOnlyElement(_git.log().setMaxCount(1).call());
         return logEntry.getId().toString() + ": " + logEntry.getShortMessage();
     }
 
@@ -67,7 +68,7 @@ public class GitWrapper {
      * @return successful commit's SHA-1
      */
     public String getLastLogSha1() throws GitAPIException {
-        RevCommit logEntry = Iterables.getOnlyElement(_git.log().setMaxCount(1).call());
+        RevCommit logEntry = getOnlyElement(_git.log().setMaxCount(1).call());
         return ObjectId.toString(logEntry);
     }
 
@@ -77,7 +78,41 @@ public class GitWrapper {
      * @return successful commit's short message
      */
     public String getLastLogMessage() throws GitAPIException {
-        RevCommit logEntry = Iterables.getOnlyElement(_git.log().setMaxCount(1).call());
+        RevCommit logEntry = getOnlyElement(_git.log().setMaxCount(1).call());
         return logEntry.getShortMessage();
+    }
+
+    public String getHeadSha1() throws IOException {
+        return ObjectId.toString(_git.getRepository().resolve("HEAD"));
+    }
+
+    public String getCurrentBranchName() throws IOException {
+        return _git.getRepository().getBranch();
+    }
+
+    public String createBranch(String branchName) throws GitAPIException {
+        Ref ref = _git.checkout().setCreateBranch(true).setName(branchName).call();
+        return ObjectId.toString(ref.getObjectId());
+    }
+
+    public String checkOutBranch(String branchName) throws GitAPIException, IOException {
+        _git.checkout().setName(branchName).call();
+        return getHeadSha1();
+    }
+
+    public String checkoutMasterAndDeleteBranch(String branchName) throws GitAPIException {
+        _git.checkout().setName("master").call();
+        return getOnlyElement(_git.branchDelete().setForce(true).setBranchNames(branchName).call());
+    }
+
+    public boolean doesBranchExist(String branchName) throws GitAPIException {
+        return findBranchByName(branchName).isPresent();
+    }
+
+    private Optional<Ref> findBranchByName(String branchName) throws GitAPIException {
+        List<Ref> branches = _git.branchList().call();
+        return branches.stream()
+                .filter(branch -> branch.getName().equals("refs/heads/" + branchName))
+                .findFirst();
     }
 }
--- src/test/java/com/example/jgit/GitWrapperTest.java
+++ src/test/java/com/example/jgit/GitWrapperTest.java
@@ -1,28 +1,29 @@
 package com.example.jgit;
 
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.util.FileUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GitWrapperTest {
 
-    private static File _tempDir;
+    private static final String TEST_BRANCH = "TEST_Branch";
+    private File _tempDir;
 
-    @BeforeAll
-    public static void setUp() throws IOException {
+    @BeforeEach
+    public void setUp() throws IOException {
         _tempDir = Files.createTempDirectory(GitWrapperTest.class.getSimpleName()).toFile();
     }
 
-    @AfterAll
-    public static void tearDown() throws IOException {
+    @AfterEach
+    public void tearDown() throws IOException {
         FileUtils.delete(_tempDir, FileUtils.RECURSIVE | FileUtils.IGNORE_ERRORS);
     }
 
@@ -47,5 +48,61 @@ public class GitWrapperTest {
         assertEquals(logMessage, sut.getLastLogMessage());
         assertTrue(sut.getLastLogEntry().contains(sha1));
         assertTrue(sut.getLastLogEntry().contains(logMessage));
+    }
+
+    @Test
+    public void test_that_GitWrapper_can_create_branch() throws Exception {
+        GitWrapper sut = new GitWrapper(_tempDir);
+        assertFalse(sut.doesBranchExist(TEST_BRANCH));
+        commitSomething(sut, "blah1");
+        String sha1Master = sut.getHeadSha1();
+
+        String sha1CreateBranch = sut.createBranch(TEST_BRANCH);
+        commitSomething(sut, "blah2");
+
+        assertEquals(sha1CreateBranch, sha1Master);
+        assertTrue(sut.doesBranchExist(TEST_BRANCH));
+        String sha1BranchHead = sut.getHeadSha1();
+        assertNotEquals(sha1Master, sha1BranchHead);
+    }
+
+    @Test
+    public void test_that_GitWrapper_can_delete_branch() throws Exception {
+        GitWrapper sut = new GitWrapper(_tempDir);
+        commitSomething(sut);
+        String sha1Master = sut.getHeadSha1();
+        sut.createBranch(TEST_BRANCH);
+        sut.checkOutBranch(TEST_BRANCH);
+        assertTrue(sut.doesBranchExist(TEST_BRANCH));
+
+        String actual = sut.checkoutMasterAndDeleteBranch(TEST_BRANCH);
+
+        assertFalse(sut.doesBranchExist(TEST_BRANCH));
+        assertEquals("refs/heads/" + TEST_BRANCH, actual);
+        assertEquals(sha1Master, sut.getHeadSha1());
+    }
+
+    @Test
+    public void test_getHeadSha1() throws Exception {
+        GitWrapper sut = new GitWrapper(_tempDir);
+        String sha1 = commitSomething(sut);
+        assertEquals("master", sut.getCurrentBranchName());
+        String logSha1 = sut.getLastLogSha1();
+        assertEquals(sha1, logSha1);
+
+        String masterBranchTipSha1 = sut.getHeadSha1();
+
+        assertEquals(masterBranchTipSha1, sha1);
+    }
+
+    private String commitSomething(GitWrapper sut) throws IOException, GitAPIException {
+        return commitSomething(sut, "blah");
+    }
+
+    private String commitSomething(GitWrapper sut, String fileNamePrefix) throws IOException, GitAPIException {
+        assertTrue(new File(_tempDir, fileNamePrefix + ".txt").createNewFile());
+        String logMessage = getClass().getSimpleName() + ": committing a txt file";
+        sut.add("*.txt");
+        return sut.commit(logMessage);
     }
 }
