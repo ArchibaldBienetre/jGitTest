4811ef5b8d8443f860c61d828516571f3707203a
hoobert <bluewhobert@gmail.com>
2018-05-04 08:28:51 +0200

    remove redundant proxy from test

--- src/test/java/com/example/jgit/impl/ThrowingGitWrapperImplTest.java
+++ src/test/java/com/example/jgit/impl/ThrowingGitWrapperImplTest.java
@@ -4,24 +4,10 @@ import com.example.jgit.ThrowingGitWrapper;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 public class ThrowingGitWrapperImplTest extends AbstractGitWrapperImplTest<ThrowingGitWrapper> {
     @Override
     protected ThrowingGitWrapper createGitWrapper() throws IOException, GitAPIException {
-        ThrowingGitWrapper wrapper = ThrowingGitWrapperImpl.createForLocalOnlyRepository(_tempDir);
-        return (ThrowingGitWrapper) Proxy.newProxyInstance(getClass().getClassLoader(), new Class<?>[]{ThrowingGitWrapper.class}, new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                try {
-                    return method.invoke(wrapper, args);
-                } catch (InvocationTargetException e) {
-                    throw e.getCause();
-                }
-            }
-        });
+        return ThrowingGitWrapperImpl.createForLocalOnlyRepository(_tempDir);
     }
 }
